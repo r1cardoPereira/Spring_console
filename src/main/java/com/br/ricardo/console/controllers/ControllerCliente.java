cb2232d0d9e02585f87d524d8f372c716cbaf976
Ricardo Pereira <103537193+r1cardoPereira@users.noreply.github.com>
2022-08-26 13:31:10 -0300

    Adicionando Controllers

---
+++ src/main/java/com/br/ricardo/console/controllers/ControllerCliente.java
@@ -0,0 +1,11 @@
+package com.br.ricardo.console.controllers;
+
+import com.br.ricardo.console.models.Cliente;
+
+public class ControllerCliente {
+    Cliente nome = new Cliente();
+
+    public Cliente getNome() {
+        return nome;
+    }
+}
